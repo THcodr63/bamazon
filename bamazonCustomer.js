1ae50ae44bc16674a9bceffeee272ad864fde199
Tiara Hamilton <tnhami63@gmail.com>
2017-09-09 19:53:28 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection (
 {
   host: "localhost",
-  port: 3030,
+  port: 3306,
 
   user: "root",
   password: "root",
@@ -14,33 +14,58 @@ var connection = mysql.createConnection (
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Success");
     tableStuff();
-})
+});
 
 var tableStuff = function() {
     connection.query("SELECT * FROM products", function(err, res) {
         for(var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " || " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity);
+            console.log(res[i].item_id + " || " + res[i].product_name + " || "
+             + res[i].department_name + " || " + res[i].price + " || " 
+             + res[i].stock_quantity);
         }
-        promptCustomer(res);
-    })
-
+      questPrompt(res);
+    });
 }
-    
-var promptCustomer = function(res) {
+
+var questPrompt = function(res) {
     inquirer.prompt ([{
         type: "input",
-        name: "Choice",
+        name: "purchase",
         message: "What product would you like to purchase?"
     }]).then(function(answer) {
         var correct = false;
         for(var i = 0; i < res.length; i++) {
-            if(res[i].product_name === answer.choice) {
+            if(res[i].product_name === answer.purchase) {
                 correct === true;
-                var product = answer.choice;
+                var product = answer.purchase;
                 var id = i;
-            }
+
+            inquirer.prompt ({
+                type: "input",
+                name: "quantity",
+                message: "How many would you like?",
+                validate: function(value){
+                    if(isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }).then(function(answer) {
+                if((res[id].stock_quantity-answer.quantity) > 0) {
+                    connection.query("UPDATE products SET stock_quantity = '" + (res[id].stock_quantity-answer.quantity) + 
+                    "WHERE product_name =" + product + "", 
+                    function(err, res2){
+                        console.log("Product Purchased!");
+                        tableStuff();
+                    })
+               } else {
+                   console.log("Selection invalid!");
+                   questPrompt(res);
+               }
+            });
         }
-    })
+      }
+  });
 }
